875e7aaf5eae47fc4abce8488d64955fd079dc22
dongdog507 <donggae0507@gmail.com>
2026-01-12 20:14:42 +0900

    no message

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
 print("Hello, FlyAI!")
 
 for i in range(5):
+<<<<<<< HEAD
     for j in range(7):
         print("X", end=" ")
     print()
+=======
+    for j in range(5):
+        print("0", end=" ")
+    print()
+>>>>>>> parent of bebc9ff (5 -> 7 수정)
